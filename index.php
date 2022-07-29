a26561a0f8d1850f83c7dfb779111e6bb61b9117
eklaut-koeln <67636377+eklaut-koeln@users.noreply.github.com>
2022-07-29 10:44:49 +0000

    Update index.php
    New Name-
    Added Mastodon.

--- index.php
+++ index.php
@@ -3,10 +3,15 @@
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title>Eklaut-Köln</title>
+		<title>Das Eichhörn</title>
 		<link rel="stylesheet" type="text/css" href="ww.css" />
 	</head>
 	<body>
-		<h1>Eklaut-Köln</h1>
+		<h1>Das Eichhörn</h1>
+		<ul>
+			<li><a rel="me" href="https://social.tchncs.de/@ehkoeln">Mastodon</a></li>
+			<li></li>
+			<li></li>
+		</ul>
 	</body>
 </html>
